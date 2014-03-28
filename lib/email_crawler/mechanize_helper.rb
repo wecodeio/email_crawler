f2aba8313304f71c280d101c7e5e3d0bfd635bc2
Cristian Rasch <cristianrasch@gmail.com>
2014-03-28 10:23:52 -0300

    Timeout HTTP connections

--- lib/email_crawler/mechanize_helper.rb
+++ lib/email_crawler/mechanize_helper.rb
@@ -2,10 +2,12 @@ require "mechanize"
 
 module EmailCrawler
   module MechanizeHelper
+    READ_TIMEOUT = 15
+
     def new_agent
       Thread.current[:agent] ||= Mechanize.new do |agent|
         agent.user_agent_alias = "Mac Safari"
-        agent.open_timeout = agent.read_timeout = 30
+        agent.open_timeout = agent.read_timeout = READ_TIMEOUT
         agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
         agent.history.max_size = 1
         yield(agent) if block_given?
@@ -16,10 +18,19 @@ module EmailCrawler
       retried = false
 
       begin
-        page = agent.get(url)
+        page = begin
+                 Timeout::timeout(READ_TIMEOUT) do
+                   agent.get(url)
+                 end
+               rescue Timeout::Error
+                 unless retried
+                   retried = true
+                   retry
+                 end
+               end
         page if page.is_a?(Mechanize::Page)
       rescue Mechanize::Error;
-      rescue SocketError, Net::OpenTimeout
+      rescue SocketError
         unless retried
           retried = true
           retry
